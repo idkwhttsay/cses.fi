0386175711504d46f6c4dbbc8805eeaf923d6b3b
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-01-15 17:28:35 -0500

    feat: add 2 graph problems

--- graph algorithms/building-teams.cpp
+++ graph algorithms/building-teams.cpp
@@ -1,3 +1,59 @@
-//
-// Created by Danil Tatarinov on 15/01/2025.
-//
+#include <bits/stdc++.h>
+#define ll long long
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define speed ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+using namespace std;
+ 
+const ll inf = 1e18;
+const ll N = 1e5 + 123;
+const ll mod = 1e6 + 3;
+const double PI = 3.1415926536;
+ 
+int n,m,c[N];
+vector <int> g[N];
+bool used[N];
+ 
+void dfs(int v){
+    used[v] = true;
+    for(int to: g[v]){
+        if(c[v] == c[to]){
+            cout << "IMPOSSIBLE";
+            exit(0);
+        }
+        
+        if(!used[to]){
+            c[to] = 3 - c[v];
+            dfs(to);
+        }
+    }
+}
+ 
+int main() {
+    speed;
+    cin >> n >> m;
+    for(int i = 1; i <= m; ++i){
+        int x,y;
+        cin >> x >> y;
+        g[x].pb(y);
+        g[y].pb(x);
+    }
+    
+    for(int i = 1; i <= n; ++i){
+        if(!used[i]){
+            c[i] = 1;
+            dfs(i);
+        }
+    }
+    
+    for(int i = 1; i <= n; ++i){
+        if(c[i] == 0){
+            cout << 1 << ' ';
+        } else {
+            cout << c[i] << ' ';
+        }
+    }
+}
--- graph algorithms/round-trip.cpp
+++ graph algorithms/round-trip.cpp
@@ -1,3 +1,73 @@
-//
-// Created by Danil Tatarinov on 15/01/2025.
-//
+#include <bits/stdc++.h>
+ 
+#define int long long
+ 
+using namespace std;
+ 
+const int N = 1e5+123;
+ 
+int n,m;
+vector <int> g[N];
+int used[N];
+vector <int> ans;
+vector <int> cur;
+ 
+void dfs(int v, int prev = -1) {
+    // cout << v << '\n';
+    used[v] = 1;
+    cur.push_back(v);
+ 
+    for(int to: g[v]) {
+        if(prev == to || used[to] == 2) continue;
+ 
+        if(used[to] == 1) {
+            int ind = cur.size() - 1;
+            while(true) {
+                ans.push_back(cur[ind]);
+                if(cur[ind] == to) break;
+                ind --;
+            }
+ 
+            reverse(ans.begin(), ans.end());
+            ans.push_back(to);
+            cout << ans.size() << '\n';
+            for(int i = 0; i < ans.size(); ++i) {
+                cout << ans[i] << ' ';
+            }
+ 
+            exit(0);
+        } 
+ 
+        dfs(to, v);
+    }
+ 
+    cur.pop_back();
+    used[v] = 2;
+}
+ 
+void solve() {
+    cin >> n >> m;
+    for(int i = 0; i < m; ++i) {
+        int u,v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+ 
+    for(int i = 1; i <= n; ++i) {
+        if(!used[i]) {
+            dfs(i);
+        }
+    }
+ 
+    cout << "IMPOSSIBLE";
+}
+ 
+signed main() {
+    int tt = 1;
+    // cin >> tt;
+    while(tt --) {
+        solve();
+        cout << '\n';
+    }
+}
