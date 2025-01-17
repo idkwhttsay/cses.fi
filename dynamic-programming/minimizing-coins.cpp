529dce9717e05ec8eb3b26012fc44a2c0c15d466
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-01-16 22:58:06 -0500

    feat: add minimizing coins

--- dynamic-programming/minimizing-coins.cpp
+++ dynamic-programming/minimizing-coins.cpp
@@ -1,3 +1,52 @@
-//
-// Created by Danil Tatarinov on 16/01/2025.
-//
+#include <bits/stdc++.h>
+
+#define int long long
+#define F first
+#define S second
+#define pii pair<int, int>
+
+using namespace std;
+
+const int N = 1e6+123;
+const int INF = 1e18;
+const int mod = 1e9+7;
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+int n,x;
+int c[110], dp[N];
+
+void solve() {
+    cin >> n >> x;
+    for(int i = 1; i <= n; ++i){
+        cin >> c[i];
+    }
+
+    for(int i = 0; i <= x; ++i){
+        dp[i] = INF;
+    }
+
+    dp[0] = 0;
+    for(int i = 1; i <= n; ++i){
+        for(int j = 0; j <= x-c[i]; ++j){
+            if(dp[j] != INF){
+                dp[j + c[i]] = min(dp[j + c[i]], dp[j] + 1);
+            }
+        }
+    }
+
+    if(dp[x] == INF){
+        cout << -1;
+    } else {
+        cout << dp[x];
+    }
+}
+
+signed main() {
+    int tt = 1;
+    // cin >> tt;
+    while(tt --) {
+        solve();
+        cout << '\n';
+    }
+}
